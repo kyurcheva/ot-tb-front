6838e0c8b1d0532d9e12ccdbd1cf6988c00c1b65
kyurcheva <kyurcheva@yandex.ru>
2021-03-28 01:02:34 +0300

    Add user info, icons, hash

--- js/logic.js
+++ js/logic.js
@@ -1,78 +1,119 @@
-
-$('.login-block #auth-btn').click(function () {
-    console.log('Клик на кнопку');
-});
-
 $('.dropdown').click(function(){
     $('.dropdown-menu').toggleClass('show');
     console.log("Клик на роль");
 });
 
-
-
-$('.navbar-left-nav').on('click', '.dropdown', function (){
-   console.log('fjjodj');
-});
-
-
-
-function goHome(){
+//Click LOG OUT
+$('.navbar-left-nav').on('click', '#btn-log-out', function(){
+    console.log('logout');
     $("#sign-in-link").show();
     $(".navbar-left-nav").children().last().remove();
     window.location.hash = '#home';
+});
+
+function goHome(){
+    //$("#sign-in-link").show();
+    //$(".navbar-left-nav").children().last().remove();
+    //window.location.hash = '#home';
+
+}
+
+//Отображение формы с инфо пользователя
+function infoMenu(userInfo){
+    $("#sign-in-link").hide();
+    /*$(".navbar-left-nav").append("<li id=\"info-user\" class=\"nav-item active\">\n" +
+        "                    <div class=\"dropdown\">\n" +
+        "<div class=\"btn-group dropdown\">\n" +
+        "  <button id=\"user-login-btn-info\" type=\"button\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n" +
+        "                        <span>OTTBUser <i class=\"fa fa-user-circle\"></i></span>\n" +
+        "</button>" +
+        "                       <div class=\"dropdown-menu\">\n" +
+        "                            <form id=\"form-info-user\" class=\"\">\n" +
+        "                                <div class=\"form-group\">\n" +
+        "                                    <p><u><b>Login:  </b></u>" + userInfo.username + "</p>\n" +
+        "<hr>" +
+        "                                    <p><u><b>Имя:  </b></u>" + userInfo.firstName + "</p>\n" +
+        "                                    <p><u><b>Фамилия:  </b></u>" + userInfo.lastName + "</p>\n" +
+        "                                    <p><u><b>Отчество:  </b></u>" + userInfo.patronymic +"</p>\n" +
+        "                                    <p><u><b>Email:  </b></u>" + userInfo.email +"</p>\n" +
+        "                                </div>\n" +
+
+
+        "                                <div class=\"form-group btn-log-out\">\n" +
+        "                                    <button type=\"button\" class=\"btn btn-primary\">Log out</button>\n" +
+        "                                </div>\n" +
+        "                            </form>\n" +
+        "                    </div>\n" +
+        "                </li>");*/
+
+
+    $(".navbar-left-nav").append("<li id=\"info-user\" class=\"nav-item active\">\n" +
+        "                    <div class=\"dropdown\">\n" +
+        "<div class=\"btn-group dropdown\">\n" +
+        "  <button id=\"user-login-btn-info\" type=\"button\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n" +
+        "                        <span>" + userInfo.username +" <i id=\"icon-user\" class=\"fa fa-user-circle\"></i></span>\n" +
+        "</button>" +
+        "                       <div class=\"dropdown-menu\">\n" +
+        "                            <form id=\"form-info-user\" class=\"\">\n" +
+        "                               <div class=\"row\">\n" +
+        "                                  <div class=\"col\">" +
+        "<b>" + userInfo.firstName + " " +  userInfo.lastName + " " + userInfo.patronymic + "</b>\n" +
+        "                                      <hr>" +
+        "                                  </div>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"row\">\n" +
+        "                                  <div class=\"col col-3\">\n" +
+        "                                       Login:\n" +
+        "                                  </div>\n" +
+        "                                  <div class=\"col col-9\">\n" +
+        userInfo.username +
+        "                                  </div>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"row\">\n" +
+        "                                    <div class=\"col-3\">\n" +
+        "                                        Email:\n" +
+        "                                    </div>\n" +
+        "                                    <div class=\"col-9\">\n" +
+        userInfo.email +
+        "                                    </div>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"row\">\n" +
+        "                                    <div class=\"col-3\">\n" +
+        "                                        Role:\n" +
+        "                                    </div>\n" +
+        "                                    <div class=\"col-9\">\n" +
+        userInfo.role +
+        "                                    </div>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"row\">\n" +
+        "                                     <div class=\"col btn-log-out\">\n" +
+        "                                       <button id=\"btn-log-out\" type=\"button\" class=\"btn btn-primary\">Log out</button>\n" +
+        "                                     </div>\n" +
+        "                                 </div>" +
+        "                            </form>\n" +
+        "                    </div>\n" +
+        "                </li>");
 }
 
 
 
+//Выбор домашней страницы в соответсвии с ролью
 function loadHomePage(userInfo) {
     switch (userInfo.role) {
         case 'ADMIN':
             window.location.hash = '#adminHomePage';
-            $("#sign-in-link").hide();
-            $(".navbar-left-nav").append("<li class=\"nav-item active\">\n" +
-                "                    <a id=\"user-icon\" class=\"navbar-brand\" href=\"#\">\n" +
-                "                        <span>Admin <i class=\"fa fa-user-circle\"></i></span>\n" +
-                "                    </a>\n" +
-                "                </li>");
-
-
+            infoMenu(userInfo);
             break;
         case 'OTTB_USER':
-            window.location.hash = '#OTTBHomePage';
-            $("#sign-in-link").hide();
-        /*$(".navbar-left-nav").append("<li class=\"nav-item active\">\n" +
-            "                    <a id=\"user-icon\" class=\"navbar-brand\" href=\"#\">\n" +
-            "                        <span>OTTBUser <i class=\"fa fa-user-circle\"></i></span>\n" +
-            "                    </a>\n" +
-            "                </li>");*/
-           $(".navbar-left-nav").append("<li id=\"info-user\" class=\"nav-item active\">\n" +
-               "                    <div class=\"dropdown\">\n" +
-               "<div class=\"btn-group dropleft\">\n" +
-               "  <button id=\"user-login-btn-info\" type=\"button\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n" +
-               "                        <span>OTTBUser <i class=\"fa fa-user-circle\"></i></span>\n" +
-               "</button>" +
-                "                       <div class=\"dropdown-menu\">\n" +
-               "                            <form id=\"form-info-user\" class=\"\">\n" +
-               "                                <div class=\"form-group\">\n" +
-               "                                    <p>Login: " + userInfo.username + "</p>\n" +
-               "                                    <p>Имя: " + userInfo.firstName + "</p>\n" +
-               "                                    <p>Фамилия: " + userInfo.lastName + "</p>\n" +
-               "                                    <p>Отчество: " + userInfo.patronymic +"</p>\n" +
-               "                                </div>\n" +
-               "                                <div class=\"form-group\">\n" +
-               "                                    <p>Email: " + userInfo.email +"</p>\n" +
-               "                                <button type=\"button\" class=\"btn btn-primary\">Log out</button>\n" +
-               "                            </form>\n" +
-               "                        </div>" +
-               "                    </div>\n" +
-               "                </li>");
+           window.location.hash = '#OTTBHomePage';
+           infoMenu(userInfo);
            break;
 
    }
     }
 
 
-
+//Авторизация
 function auth() {
     console.log('Клик на кнопку2');
 
@@ -98,8 +139,15 @@ function auth() {
                     console.log(responseInfo);
                     console.log(responseInfo.role);
                     loadHomePage(responseInfo);
+                },
+                error: function(e){
+                    alert('Ошибка сервера');
                 }
             });
+        },
+        error: function (e){
+            alert('Неправильный логин или пароль! Попробуйте еще раз');
+            $('#auth')[0].reset();
         }
     });
 }
