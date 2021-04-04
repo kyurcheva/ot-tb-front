395356a518d02f240cb185a0116b750abd64f688
kyurcheva <kyurcheva@yandex.ru>
2021-04-04 17:28:20 +0300

    Страницы шаблона, создания шаблона, списка виртуальных столов,создание виртуального стола, отображение виртуального стола

--- js/app.js
+++ js/app.js
@@ -3,10 +3,15 @@
 (function () {
     function init() {
         var router = new Router([
-            new Route('home', 'home.html', true),
-            new Route('authorization', 'authorization.html'),
+            //new Route('home', 'home.html', true),
+            new Route('authorization', 'authorization.html', true),
             new Route('adminHomePage', 'adminHomePage.html'),
-            new Route('OTTBHomePage', 'OTTBHomePage.html')
+            new Route('OTTBHomePage', 'OTTBHomePage.html'),
+            new Route('templateDesktop', 'templateDesktop.html'),
+            new Route('createTemplate', 'createTemplate.html'),
+            new Route('existingTables', 'existingTables.html'),
+            new Route('createDesktop', 'createDesktop.html'),
+            new Route('desktop', 'desktop.html')
         ]);
     }
     init();
--- js/logic.js
+++ js/logic.js
@@ -3,6 +3,13 @@ $('.dropdown').click(function(){
     console.log("Клик на роль");
 });
 
+window.addEventListener('hashchange', function(e) {
+    $('body').children('.point').remove();
+    if (window.location.hash != '#existingTables'){
+        $('body').children('.table-desktop').remove();
+    }
+});
+
 //Click LOG OUT
 $('.navbar-left-nav').on('click', '#btn-log-out', function(){
     console.log('logout');
@@ -21,32 +28,6 @@ function goHome(){
 //Отображение формы с инфо пользователя
 function infoMenu(userInfo){
     $("#sign-in-link").hide();
-    /*$(".navbar-left-nav").append("<li id=\"info-user\" class=\"nav-item active\">\n" +
-        "                    <div class=\"dropdown\">\n" +
-        "<div class=\"btn-group dropdown\">\n" +
-        "  <button id=\"user-login-btn-info\" type=\"button\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n" +
-        "                        <span>OTTBUser <i class=\"fa fa-user-circle\"></i></span>\n" +
-        "</button>" +
-        "                       <div class=\"dropdown-menu\">\n" +
-        "                            <form id=\"form-info-user\" class=\"\">\n" +
-        "                                <div class=\"form-group\">\n" +
-        "                                    <p><u><b>Login:  </b></u>" + userInfo.username + "</p>\n" +
-        "<hr>" +
-        "                                    <p><u><b>Имя:  </b></u>" + userInfo.firstName + "</p>\n" +
-        "                                    <p><u><b>Фамилия:  </b></u>" + userInfo.lastName + "</p>\n" +
-        "                                    <p><u><b>Отчество:  </b></u>" + userInfo.patronymic +"</p>\n" +
-        "                                    <p><u><b>Email:  </b></u>" + userInfo.email +"</p>\n" +
-        "                                </div>\n" +
-
-
-        "                                <div class=\"form-group btn-log-out\">\n" +
-        "                                    <button type=\"button\" class=\"btn btn-primary\">Log out</button>\n" +
-        "                                </div>\n" +
-        "                            </form>\n" +
-        "                    </div>\n" +
-        "                </li>");*/
-
-
     $(".navbar-left-nav").append("<li id=\"info-user\" class=\"nav-item active\">\n" +
         "                    <div class=\"dropdown\">\n" +
         "<div class=\"btn-group dropdown\">\n" +
@@ -151,3 +132,202 @@ function auth() {
         }
     });
 }
+const plus = '+';
+const min = '-';
+
+function appendDom(container, jsonData) {
+    for (var i = 0; i <jsonData.length; i++) {
+        /*var $divParent  = $("<ul class='elem'><li></li></ul>");
+        $divParent.children().last().text(jsonData[i].name).attr('id',jsonData[i].name );
+        if (jsonData[i].children) {
+            appendDom($divParent.children().last(), jsonData[i].children);
+        }*/
+
+        var $divParent  = $("<div class='point'><ch>-</ch><span></span></div>");
+        $divParent.attr('id',jsonData[i].name );
+        $divParent.find('span').text(jsonData[i].name);
+        if (jsonData[i].children) {
+            appendDom($divParent, jsonData[i].children);
+        }
+        container.append($divParent);
+    }
+}
+
+
+var json22 = {
+    "name": "template",
+    "organization": "all",
+    "creationDate": "2021-04-01T20:28:57.776+00:00",
+    "modifiedDate": "2021-04-01T20:28:57.776+00:00",
+    "folderStructure": [
+    {
+        "name": "element1",
+        "children": [
+            {
+                "name": "element1.1",
+                "children": [
+                    {
+                        "name": "element1.1.1",
+                        "children": null,
+                        "creationDate": "2021-04-01T20:28:57.776+00:00",
+                        "modifiedDate": "2021-04-01T20:28:57.776+00:00"
+                    }
+                ],
+                "creationDate": "2021-04-01T20:28:57.776+00:00",
+                "modifiedDate": "2021-04-01T20:28:57.776+00:00"
+            },
+            {
+                "name": "element1.2",
+                "children": null,
+                "creationDate": "2021-04-01T20:28:57.776+00:00",
+                "modifiedDate": "2021-04-01T20:28:57.776+00:00"
+            }
+        ],
+        "creationDate": "2021-04-01T20:28:57.776+00:00",
+        "modifiedDate": "2021-04-01T20:28:57.776+00:00"
+    },
+    {
+        "name": "element2",
+        "children": null,
+        "creationDate": "2021-04-01T20:28:57.776+00:00",
+        "modifiedDate": "2021-04-01T20:28:57.776+00:00"
+    },
+    {
+        "name": "element3",
+        "children": null,
+        "creationDate": "2021-04-01T20:28:57.776+00:00",
+        "modifiedDate": "2021-04-01T20:28:57.776+00:00"
+    },
+    {
+        "name": "element4",
+        "children": null,
+        "creationDate": "2021-04-01T20:28:57.776+00:00",
+        "modifiedDate": "2021-04-01T20:28:57.776+00:00"
+    },
+    {
+        "name": "element5",
+        "children": null,
+        "creationDate": "2021-04-01T20:28:57.776+00:00",
+        "modifiedDate": "2021-04-01T20:28:57.776+00:00"
+    }
+]
+};
+
+//////////////////При нажатии на шаблон виртуальных столов
+
+var templateMarker = false;
+$(document).on('click', '#template-point', function(e) {
+    //По хорошему, тут нужно сначала отправить запрос. И есть вернется null отображать диалоговое окно
+    if (templateMarker) {
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:8080/template",
+            success: function (response) {
+                console.log(response);
+                window.location.hash = '#templateDesktop';
+                console.log('Rjkfnnggd');
+                console.log(response.folderStructure);
+                appendDom($('body'), response.folderStructure);
+            },
+            error: function (e) {
+                alert('Ошибка сервера');
+            }
+        });
+    } else{
+        if (confirm('Шаблон отсутствует. Хотите создать?')){
+            console.log("страница");
+            window.location.hash = "#createTemplate";
+        } else{
+            console.log("отмена");
+        }
+    }
+
+});
+
+///Шаблон виртуального стола, клик по элементу. Свернуть\развернуть список
+$(document).on('click', 'span', function(e) {
+        if ($(e.target).parent().children('.point').hasClass('hide')){
+            $(e.target).parent().children('.point').show();
+            $(e.target).parent().children('.point').removeClass('hide');
+            if ($(e.target).parent().children('.point').length > 0){
+                $(e.target).parent().children('ch').text('-');
+            }
+        } else{
+            $(e.target).parent().children('.point').hide();
+            $(e.target).parent().children('.point').addClass('hide');
+
+            if ($(e.target).parent().children('.point').length > 0){
+                $(e.target).parent().children('ch').text('+');
+            }
+        }
+});
+
+
+//Клик по "Список вирутальных столов"
+$(document).on('click', '#list-desktop-point', function(e) {
+    window.location.hash = "#existingTables";
+    $('body').append("<div class=\"content-width-2 table-desktop\">" +
+        "<table class=\"table\">\n" +
+        "  <thead>\n" +
+        "    <tr>\n" +
+        "      <th scope=\"col\">Название виртуального стола</th>\n" +
+        "      <th scope=\"col\">Субподрядная организация</th>\n" +
+        "      <th scope=\"col\">Дата создания</th>\n" +
+        "      <th scope=\"col\"></th>\n" +
+        "    </tr>\n" +
+        "  </thead>\n" +
+        "  <tbody>\n" +
+        "    <tr>\n" +
+        "      <th scope=\"row\"><a class=\"name-desktop\">Виртуальный стол 1</a></th>\n" +
+        "      <td>Организация 1</td>\n" +
+        "      <td>04.04.2021</td>\n" +
+        "      <td><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i><ch> | </ch><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></td>\n" +
+        "    </tr>\n" +
+        "    <tr>\n" +
+        "      <th scope=\"row\"><a class=\"name-desktop\">Виртуальный стол 2</a></th>\n" +
+        "      <td>Организация 2</td>\n" +
+        "      <td>03.04.2021</td>\n" +
+        "      <td><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i><ch> | </ch><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></td>\n" +
+        "    </tr>\n" +
+        "    <tr>\n" +
+        "      <th scope=\"row\"><a class=\"name-desktop\">Виртуальный стол 3</a></th>\n" +
+        "      <td>Организация 3</td>\n" +
+        "      <td>02.04.2021</td>\n" +
+        "      <td><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i><ch> | </ch><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></td>\n" +
+        "    </tr>\n" +
+        "    <tr>\n" +
+        "      <th scope=\"row\"><button id=\"create-desktop\" type=\"button\" class=\"btn btn-primary\">Создать новый <i class=\"fa fa-plus\" aria-hidden=\"true\"></i></button></th>\n" +
+        "      <td></td>\n" +
+        "      <td></td>\n" +
+        "      <td></td>\n" +
+        "    </tr>\n" +
+        "  </tbody>\n" +
+        "</table>" +
+        "</div>");
+});
+
+$(document).on('click', '.name-desktop', function(e) {
+    window.location.hash = "#desktop";
+});
+
+
+//Клик на кнопку "Создать новый виртуальный стол"
+$(document).on('click', '#create-desktop', function(e) {
+   window.location.hash = "#createDesktop";
+    console.log('Create desktop');
+});
+
+//Клик на кнопку "Сохранить шаблон"
+$(document).on('click', '#save-template', function(e) {
+    window.location.hash = "#templateDesktop";
+});
+//Клик на кнопку "Отмена сохранения"
+$(document).on('click', '#cancel-template', function(e) {
+    window.location.hash = "#adminHomePage";
+});
+/*
+$(document).on('click', '#save-desktop', function(e) {
+    window.location.hash = "#desktop";
+});
+*/
+//console.log($); // Проверка, загружена ли вообще jQuery
